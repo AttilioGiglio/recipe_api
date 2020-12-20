d20d6b9d7eb23f6eaef3f85d4c41fff6a7853bed
attiliogiglio <attilio.g93@gmail.com>
2020-12-19 22:38:48 -0300

    " Please enter the commit message for your changes. Lines starting

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,15 +1,16 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
         email = 'test@londonappdev.com'
-        password = 'Testpass123'
+        password = 'Password123'
         user = get_user_model().objects.create_user(
-            email = email,
-            password = password
+            email=email,
+            password=password
         )
 
         self.assertEqual(user.email, email)
@@ -17,7 +18,7 @@ class ModelTests(TestCase):
 
     def test_new_user_email_normalized(self):
         """Test the email for a new user is normalized"""
-        email = 'test@LONDONAPPDEV.COM'
+        email = 'test@LONDONAPPDEV.com'
         user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
@@ -26,12 +27,13 @@ class ModelTests(TestCase):
         """Test creating user with no email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'test123')
-            
-    def test_create_new_superuser(self):
+
+    def test_new_superuser(self):
+        """Test creating a new superuser"""
         user = get_user_model().objects.create_superuser(
             'test@londonappdev.com',
             'test123'
-        ) 
+        )
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
